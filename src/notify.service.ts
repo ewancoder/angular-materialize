b19ac00c0f64139ce94f09f80fc13f0df43066be
ewancoder <ewancoder@gmail.com>
2017-01-28 17:58:44 +0300

    Log initialization

--- src/notify.service.ts
+++ src/notify.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { LoggerService } from 'ewancoder-angular-logger';
 
 declare var Materialize: any;
 
 @Injectable()
 export class NotifyService {
+    constructor(logger: LoggerService) {
+        logger.log('Initialized NotifyService.', ['notify', 'service', 'materialize', 'init']);
+    }
+
     success(message: string): void {
         Materialize.toast(message, 4000, 'green');
     }
