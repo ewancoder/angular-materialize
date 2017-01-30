2eb5852693a1bbd4757a873baf21594f2bf10528
ewancoder <ewancoder@gmail.com>
2017-01-30 19:47:05 +0300

    Export component

--- src/materialize.module.ts
+++ src/materialize.module.ts
@@ -1,10 +1,12 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { MaterializeModule as A2M } from 'angular2-materialize';
 import { NotifyService } from './notify.service';
+import { NavbarComponent } from './navbar.component';
 
 @NgModule({
     imports: [ A2M ],
-    exports: [ A2M ]
+    declarations: [ NavbarComponent ],
+    exports: [ A2M, NavbarComponent ]
 })
 export class MaterializeModule {
     static forRoot(): ModuleWithProviders {
