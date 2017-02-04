f54b5da20218bc1f2ad77116940beac9ced48641
ewancoder <ewancoder@gmail.com>
2017-02-04 14:19:41 +0300

    Add loading component

--- src/materialize.module.ts
+++ src/materialize.module.ts
@@ -1,12 +1,24 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MaterializeModule as A2M } from 'angular2-materialize';
 import { NotifyService } from './notify.service';
+import { LoadingComponent } from './loading.component';
 import { NavbarComponent } from './navbar.component';
 
 @NgModule({
-    imports: [ A2M ],
-    declarations: [ NavbarComponent ],
-    exports: [ A2M, NavbarComponent ]
+    imports: [
+        CommonModule,
+        A2M
+    ],
+    declarations: [
+        LoadingComponent,
+        NavbarComponent
+    ],
+    exports: [
+        A2M,
+        LoadingComponent,
+        NavbarComponent
+    ]
 })
 export class MaterializeModule {
     static forRoot(): ModuleWithProviders {
